1a4b29996f9b502a4b5f9511583b96f5c0ad3317
trevor-ledbetter <49376992+trevor-ledbetter@users.noreply.github.com>
2019-11-06 20:22:42 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import tweepy
+from flask import Flask, render_template
+app = Flask('Wuphf')
+
+@app.route("/")
+def main():
+	return render_template('index.html')
+	
+if __name__ == "__main__":
+	app.run()
+
+
